bb4c3cc8f34bd6b1fcf0741f443d916d9816b11d
xl000 <l_xb@foxmail.com>
2021-05-20 15:25:53 +0800

    I/O

--- my-redis/examples/hello-tokio.rs
+++ my-redis/examples/hello-tokio.rs
@@ -1,20 +1,10 @@
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::sync::mpsc;
 use tokio::task;
 
-async fn say_world() {
-    println!("world");
-}
-
-fn sync_work() -> &'static str {
-    "value"
-}
-
-async fn async_work() -> &'static str {
-    "value"
-}
-
 #[tokio::main]
-async fn main() {
+async fn main() -> io::Result<()> {
     let handle = task::spawn(async { "task::spawn" });
 
     let op = say_world();
@@ -47,4 +37,33 @@ async fn main() {
     while let Some(message) = rx.recv().await {
         println!("RECV {}", message);
     }
+
+    println!("{:?}", std::env::current_dir()?);
+    println!("{:?}", std::env::current_exe()?);
+    let exe_dir = std::env::current_exe()?.parent().unwrap().to_owned();
+    std::env::set_current_dir(exe_dir)?;
+    println!("{:?}", std::env::current_dir()?);
+
+    let mut file = File::create("foo.txt").await?;
+    file.write_all(b"some bytes").await?;
+    println!("Wrote 'some bytes'");
+
+    let mut file = File::open("foo.txt").await?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).await?;
+    println!("{:?}", buffer);
+
+    Ok(())
+}
+
+async fn say_world() {
+    println!("world");
+}
+
+fn sync_work() -> &'static str {
+    "value"
+}
+
+async fn async_work() -> &'static str {
+    "value"
 }
